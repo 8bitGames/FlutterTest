e0440a13d37e9ed2b076e051327aeb26a490ecb1
jaepil <sjpjjang11@naver.com>
2023-05-01 12:58:01 +0900

    start

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:meditest/screens/home_screen.dart';
 
 void main() {
   runApp(const App());
@@ -11,28 +10,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        textTheme: const TextTheme(
-          displayLarge: TextStyle(
-            color: Color(0xFF232B55),
-          ),
-        ),
-        cardColor: const Color(0xFFF4EDDB),
-        colorScheme: const ColorScheme(
-          background: Color(0xFFE7626C),
-          brightness: Brightness.light,
-          primary: Colors.black,
-          onPrimary: Colors.white,
-          secondary: Colors.black,
-          onSecondary: Colors.white,
-          error: Colors.red,
-          onError: Colors.white,
-          onBackground: Colors.white,
-          surface: Colors.white,
-          onSurface: Colors.black,
-        ),
-      ),
-      home: const HomeScreen(),
+      home: Container(),
     );
   }
 }
